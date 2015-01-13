36e653c7f5fcebc28772d09ddbc43fe53f2c729a
vminx <470132216@qq.com>
2015-01-13 11:02:35 +0800

    slide

---
+++ less/widget/list/ui.slide.js
@@ -0,0 +1,8 @@
+TouchSlide({ 
+	slideCell:"#J_slide",
+	titCell:".ui-slide-hd ul", //开启自动分页 autoPage:true ，此时设置 titCell 为导航元素包裹层
+	mainCell:".ui-slide-bd ul", 
+	effect:"leftLoop", 
+	autoPlay:true,//自动播放
+	autoPage:true //自动分页
+});
